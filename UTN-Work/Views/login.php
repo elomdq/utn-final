696e643673def8106622214f80d14c8639edcc31
elomdq <eloymrp@gmail.com>
2021-11-04 23:20:20 -0300

    Avances con metodo de Registro de Estudiante - Eloy

--- UTN-Work/Views/login.php
+++ UTN-Work/Views/login.php
@@ -14,7 +14,7 @@
                         <div class="row  justify-content-center">
                             <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
                             <div class="col-lg-10">
-                                <div class="px-2 py-5">
+                                <div class="px-2 py-4">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Bienvenido!</h1>
                                     </div>
@@ -26,27 +26,31 @@
                                                 placeholder="Ingrese su email para iniciar sesión...">
                                         </div>
 
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password" placeholder="Contraseña">
+                                        </div>
+
                                         <div class="form-group d-flex justify-content-around py-3">
 
-                                        <div align="center">
-                                            <input type="radio" id="student" name="userType" value="0" class="d-block" required>
-                                            <label for="student" class="">Estudiante</label>
-                                        </div>
+                                            <div align="center">
+                                                <input type="radio" id="student" name="userType" value="0" class="d-block" required>
+                                                <label for="student" class="">Estudiante</label>
+                                            </div>
 
-                                        <div class="" align="center">
-                                            <input type="radio" id="admin" name="userType" value="1" class="d-block">
-                                            <label for="admin" class="">Administrador</label>     
+                                            <div class="" align="center">
+                                                <input type="radio" id="admin" name="userType" value="1" class="d-block">
+                                                <label for="admin" class="">Administrador</label>     
+                                            </div>
+                                        
+                                            <div align="center">
+                                                <input type="radio" id="company" name="userType" value="2" class="d-block">
+                                                <label for="company" class="">Empresa</label>
+                                            </div>
+                                        
                                         </div>
                                         
-                                        <div align="center">
-                                            <input type="radio" id="company" name="userType" value="2" class="d-block">
-                                            <label for="company" class="">Empresa</label>
-                                        </div>
-                                        </div>
-                                        <!--<div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
-                                        </div>
+                                        <!--
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
@@ -66,10 +70,10 @@
                                     <!-- <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
                                     </div> -->
+                                    <div class="text-center mt-4 mb-0">
+                                        <a class="small" href="<?php echo FRONT_ROOT . "home/checkEmail"?>" >Registrarse</a>
+                                    </div> 
                                 </div>
                             </div>
                         </div>
