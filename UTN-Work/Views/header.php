a4a641e185791ccb50bf6af3e7b62167be78e8c9
elomdq <eloymrp@gmail.com>
2021-11-11 23:30:37 -0300

    inicio de aplicacion de styles - Eloy

--- UTN-Work/Views/header.php
+++ UTN-Work/Views/header.php
@@ -5,16 +5,24 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-<link href="<?php echo VENDOR_PATH . "/fontawesome-free/css/all.min.css"; ?>" rel="stylesheet" type="text/css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+<link href="<?php echo VIEWS_PATH?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="<?php echo CSS_PATH?>style.css" rel="stylesheet" type="text/css">
+
+<!-- <link href="<?php echo VENDOR_PATH . "/fontawesome-free/css/all.min.css"; ?>" rel="stylesheet" type="text/css"> -->
+
+<!--
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-
-<!-- Custom styles for this template-->
 <link href="<?php echo CSS_PATH . "sb-admin-2.css";?>" rel="stylesheet">
-
+-->
 </head>
 
 <body class="page-top pt-5 mt-5">
--- UTN-Work/Views/login.php
+++ UTN-Work/Views/login.php
@@ -1,36 +1,31 @@
 <div class="">
 
-    <div class="container">
+<div class="container ">
+    <div class="row justify-content-center">
+        <div class="card col-6 border-0 shadow-lg">
+            <div class="card-body">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+                                    <div class="mt-3 mb-3 text-center">
+                                        <h3 class="text-center">Bienvenido!</h3>
+                                    </div> 
 
-            <div class=" col-5">
-
-                <div class="card o-hidden border-0 shadow-lg my-3">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row  justify-content-center">
-                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
-                            <div class="col-lg-10">
-                                <div class="px-2 py-4">
-
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Bienvenido!</h1>
-                                    </div>                                   
-                                    <form class="user" action="<?php echo FRONT_ROOT?>Home/login" method="post">
+                                    <form class="" action="<?php echo FRONT_ROOT?>Home/login" method="post">
                                         
-                                        <div class="form-group">
+                                        <div class="input-group-md my-3">
                                             <input name="email" type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                id="inputEmail"
                                                 placeholder="Ingrese su email para iniciar sesión...">
                                         </div>
-                                        <div class="form-group">
+
+                                        <div class="input-group-md my-3">
                                             <input type="password" class="form-control form-control-user"
                                                 id="password" placeholder="Contraseña" name="password">
-                                        </div>                                    
-                                        <input type="submit" class="btn btn-primary btn-user btn-block" value="Login">
-                                       
+                                        </div> 
+                                        
+                                        <div class="input-group-lg my-3 col text-center">
+                                            <button type="submit" class="btn btn-primary">Login</button>
+                                        </div>
+                                                                               
                                     </form>
 
                                     <div class="text-center mt-4 mb-0">
@@ -40,13 +35,10 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                   
+                
 
             </div>
-
         </div>
-
     </div>
-
 </div>
