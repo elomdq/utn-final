ae054e58e6736eb4cc1bd7c41e00f2ac2ffd4d8e
elomdq <eloymrp@gmail.com>
2021-11-23 22:24:47 -0300

    casi finalizado

--- UTN-Work/Views/header.php
+++ UTN-Work/Views/header.php
@@ -11,11 +11,8 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-<link href="<?php echo CSS_PATH . "bootstrap.min.css";?>" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="<?php echo CSS_PATH . "bootstrap.min.css";?>" rel="stylesheet">
 
-<!-- Custom styles for this template-->
-<!-- <link href="<?php echo CSS_PATH . "sb-admin-2.css";?>" rel="stylesheet"> -->
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- UTN-Work/Views/login.php
+++ UTN-Work/Views/login.php
@@ -1,56 +1,44 @@
-<div class="">
+<section id="login">
 
-    <div class="container">
+    <div class="row justify-content-center">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+        <div class="card col-5 o-hidden border-0 shadow-lg my-3">
 
-            <div class=" col-5">
+            <div class="card-body px-4 py-4 row justify-content-center">
 
-                <div class="card o-hidden border-0 shadow-lg my-3">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row  justify-content-center">
-                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
-                            <div class="col-lg-10">
-                                <div class="px-2 py-4">
-
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Bienvenido!</h1>
-                                    </div>                                   
-                                    <form class="user" action="<?php echo FRONT_ROOT?>Home/login" method="post">
-                                        
-                                        <div class="form-group">
-                                            <input name="email" type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Ingrese su email para iniciar sesión..." required>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="password" placeholder="Contraseña" name="password" required>
-                                        </div>                                    
-
-
-                                        <div class="alert alert-<?php if($alert!=null) echo $alert->getType();?>" role="alert"> <?php if($alert!=null) echo $alert->getMessage(); ?> </div>
-
-                                        <input type="submit" class="btn btn-primary btn-user btn-block" value="Login">
-                                       
-                                    </form>
-
-                                    <div class="text-center mt-4 mb-0">
-                                        <a class="small" href="<?php echo FRONT_ROOT . "home/checkEmail"?>" >Registrarse</a>
-                                    </div> 
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-12 text-center">
+                    <img class="" src="<?php echo FRONT_ROOT . VIEWS_PATH . "assets/utn.png"; ?>" height="80">
                 </div>
 
+                <div class="col-12 px-2 py-3">
+
+                        <form class="user" action="<?php echo FRONT_ROOT ?>Home/login" method="post">
+
+                            <div class="form-group col-12 py-2">
+                                <label for="inputEmail">Email</label>
+                                <input name="email" type="email" class="form-control form-control-user" id="inputEmail" aria-describedby="emailHelp" required>
+                            </div>
+
+                            <div class="form-group col-12 pt-2">
+                                <label for="loginPassword">Password</label>
+                                <input type="password" class="form-control form-control-user" id="loginPassword" name="password" required>
+                            </div>
+
+                            <div class="alert alert-<?php if ($alert != null) echo $alert->getType(); ?>" role="alert" <?php if ($alert == null) echo " style=\"display:none;\""; ?> > <?php if ($alert != null) echo $alert->getMessage(); ?> </div>
+
+                            <div class="col-12 pt-4 d-flex justify-content-center">
+                                <input type="submit" class="btn btn-primary btn-user btn-block" value="Login">
+                            </div>
+
+                        </form>
+
+                        <div class="text-center mt-3 mb-0">
+                            <a class="small" href="<?php echo FRONT_ROOT . "home/checkEmail" ?>">Registrarse</a>
+                        </div>
+
+                </div>
             </div>
-
         </div>
-
     </div>
 
-</div>
+</section>
